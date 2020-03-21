e0e4310d38c47dcdcfe6a5a601c2ad29981db83b
jiejietop <jiejietop@gmail.com>
2020-03-21 16:04:14 +0800

    add frist demo...

---
+++ 001/demo.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: jiejie
+ * @Github: https://github.com/jiejieTop
+ * @Date: 2020-03-21 15:55:37
+ * @LastEditTime: 2020-03-21 16:02:00
+ * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
+ */
+
+#include <iostream>
+
+int main(void)
+{
+    using namespace std;
+    std::cout << "this is my frist c plus plus demo!" << std::endl;
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
